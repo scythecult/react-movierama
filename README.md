be7eae023b74711f7c50a3fda040c3d0870f4420
scythecult <scythecult@gmail.com>
2023-01-13 17:54:57 +0300

    -initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React @redux/toolkit + React Router
+
+Приложение выбора мест в кинотеатре
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+function App() {
+  return <h1>jello</h1>;
+}
+
+export default App;
